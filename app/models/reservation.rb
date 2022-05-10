e929a13f5ec245dcb5ac535efd5d4f4267fb0200
Anwar Jamis <anwarjamis@gmail.com>
2022-05-10 16:40:39 -0400

    index new y show listos

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,5 +1,5 @@
 class Reservation < ApplicationRecord
-  belongs_to :cars
-  belongs_to :users
+  belongs_to :car
+  belongs_to :user
   validates :start_date, :end_date, :amount, presence: true
 end
