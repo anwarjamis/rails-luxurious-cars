63fe9898bddb1f6af76cc4a003f5a20560f6154e
Sebastian Jara <s.jaranilsson@gmail.com>
2022-05-10 12:42:27 -0400

    acciones de cars listas 100%

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,11 +1,11 @@
 class CarsController < ApplicationController
+  before_action set_car, only: [:show, :edit, :update, :destroy]
 
   def index
     @cars = Car.all
   end
 
   def show
-    @car = Car.find(params[:id])
   end
 
   def new
@@ -14,24 +14,21 @@ class CarsController < ApplicationController
 
   def create
     @car = Car.new(car_params)
-    @user = User.find(params[:user_id])
-    @car.user = @user
+    @user = current_user.id
+    @car.user_id = @user
     @car.save
-    redirect_to cars_path(@car)
+    redirect_to car_path(@car)
   end
 
   def edit
-    @car = Car.find(params[:id])
   end
 
   def update
-    @car = Car.find(params[:id])
-    @car.update(params[:car])
-    redirect_to cars_path(@car)
+    @car.update(car_params)
+    redirect_to car_path(@car)
   end
 
   def destroy
-    @car = Car.find(params[:id])
     @car.destroy
     redirect_to cars_path
   end
@@ -40,5 +37,9 @@ class CarsController < ApplicationController
 
   def car_params
     params.require(:car).permit(:year, :brand, :category, :model, :city, :price, :description)
+  end
+
+  def set_car
+    @car = Car.find(params[:id])
   end
 end
