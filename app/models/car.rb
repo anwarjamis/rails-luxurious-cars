9921c6673448154b4939ed5d29d75148a88b0086
Sebastian Jara <s.jaranilsson@gmail.com>
2022-05-11 16:06:58 -0400

    authorizations casi lista, problemas haciendo el delete de reservation == por arreglar

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,6 +1,6 @@
 class Car < ApplicationRecord
   belongs_to :user
-  has_many :reservations, dependent: :delete_all
+  has_many :reservations, dependent: :destroy
   validates :year, :brand, :category, :model, :city, :price, presence: true
   validates :category, inclusion: { in: ['classic', 'sport', 'SUV', 'unique', 'others'] }
 end
