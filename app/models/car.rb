27b40419037e0857c32408bce817fbbe2d958827
Sebastian Jara <s.jaranilsson@gmail.com>
2022-05-12 16:24:43 -0400

    agregando .env

--- app/models/car.rb
+++ app/models/car.rb
@@ -3,6 +3,8 @@ class Car < ApplicationRecord
   has_many :reservations, dependent: :delete_all
   validates :year, :brand, :category, :model, :city, :price, presence: true
   validates :category, inclusion: { in: ['classic', 'sport', 'SUV', 'unique', 'others'] }
+  geocoded_by :city
+  after_validation :geocode
 
   include PgSearch::Model
   pg_search_scope :search_by_brand_and_model,
