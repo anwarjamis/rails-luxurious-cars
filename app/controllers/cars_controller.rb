1dcffd9d83108e261eda8072f99656de29897d86
Anwar Jamis <anwarjamis@gmail.com>
2022-05-12 11:03:16 -0400

    no auth index

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,5 +1,6 @@
 class CarsController < ApplicationController
   before_action :set_car, only: %i[show edit update destroy]
+  skip_before_action :authenticate_user!, only: :index
 
   def index
     @cars = policy_scope(Car).order(created_at: :desc)
