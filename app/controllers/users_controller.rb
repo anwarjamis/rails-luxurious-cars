d76d4e76ea6aec61c37dc0f43016f525d52b529b
Sebastian Jara <s.jaranilsson@gmail.com>
2022-05-12 12:19:47 -0400

    pundit de user listo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,21 +3,26 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    authorize @user
   end
 
   def new
     @user = User.new
+    authorize @user
   end
 
   def create
     @user = User.new(user_params)
+    authorize @user
   end
 
   def edit
+    authorize @user
   end
 
   def update
     @user.update(user_params)
+    authorize @user
   end
 
   private
